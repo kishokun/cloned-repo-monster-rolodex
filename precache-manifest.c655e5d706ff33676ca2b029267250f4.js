d8e958911332a16c15c8ae8aa163b8f395851105
kishokun <kishorekunal0014@gmail.com>
2021-06-06 15:47:23 +0530

    Updates

---
+++ precache-manifest.c655e5d706ff33676ca2b029267250f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73cf49001da4c879fc980246399c200",
+    "url": "/cloned-repo-monster-rolodex/index.html"
+  },
+  {
+    "revision": "af8c8a49c7b0567adcc5",
+    "url": "/cloned-repo-monster-rolodex/static/css/main.8445548c.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/cloned-repo-monster-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cloned-repo-monster-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8c8a49c7b0567adcc5",
+    "url": "/cloned-repo-monster-rolodex/static/js/main.98dc4da2.chunk.js"
+  },
+  {
+    "revision": "e497f1902726036a58ce",
+    "url": "/cloned-repo-monster-rolodex/static/js/runtime-main.38e8785f.js"
+  }
+]);
